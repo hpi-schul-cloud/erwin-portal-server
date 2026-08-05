c008c5aacb647865bde42d1b9b8a3cb8f8a3ea18
Maximilian Kreuzkam <maximilian.kreuzkam@capgemini.com>
2026-08-05 08:20:31 +0200

    EW-1470: Improve logging.

--- src/modules/rollenmapping/api/rollenmapping.service.ts
+++ src/modules/rollenmapping/api/rollenmapping.service.ts
@@ -1,21 +1,24 @@
 import { Injectable } from '@nestjs/common';
-import { PersonenkontextService } from '../../personenkontext/domain/personenkontext.service.js';
-import { ServiceProviderRepo } from '../../service-provider/repo/service-provider.repo.js';
-import { Personenkontext } from '../../personenkontext/domain/personenkontext.js';
-import { ServiceProvider } from '../../service-provider/domain/service-provider.js';
+import { ClassLogger } from '../../../core/logging/class-logger.js';
 import { RolleID } from '../../../shared/types/aggregate-ids.types.js';
+import { Personenkontext } from '../../personenkontext/domain/personenkontext.js';
+import { PersonenkontextService } from '../../personenkontext/domain/personenkontext.service.js';
+import { ServiceProvider } from '../../service-provider/domain/service-provider.js';
+import { ServiceProviderRepo } from '../../service-provider/repo/service-provider.repo.js';
 
 @Injectable()
 export class RollenMappingService {
     public constructor(
         private readonly serviceProviderRepo: ServiceProviderRepo,
         private readonly personenKontextService: PersonenkontextService,
+        private readonly logger: ClassLogger,
     ) {}
 
     public async getRoleOnServiceProviderByClientName(clientName: string, userId: string): Promise<RolleID | null> {
         const serviceProvider: Option<ServiceProvider<true>> = await this.serviceProviderRepo.findByName(clientName);
 
         if (!serviceProvider) {
+            this.logger.warning(`Couldn't find serviceProvider with name ${clientName}`);
             return null;
         }
 
@@ -25,6 +28,10 @@ export class RollenMappingService {
             personenkontexte.find(
                 (pk: Personenkontext<true>) => pk.organisationId === serviceProvider.providedOnSchulstrukturknoten,
             )?.rolleId ?? null;
+
+        if (!roleId) {
+            this.logger.warning(`Couldn't find role for user ${userId} and serviceprovider ${serviceProvider.id}`);
+        }
 
         return roleId;
     }
